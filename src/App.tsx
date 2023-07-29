2ae85b606a491d0315fa3052d09a727ef4cadeed
Kfmoss <berlin1980e@gmail.com>
2023-07-29 12:32:07 +0200

    switch color mode ready

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
 
     <Show above="lg">
 
-      <GridItem area={'aside'} bg={"gold"}>Aside</GridItem>
+      <GridItem area={'aside'}>Aside</GridItem>
 
     </Show>
 
-    <GridItem area={'main'} bg={"dodgerblue"}>Main</GridItem>
+    <GridItem area={'main'}>Main</GridItem>
   </Grid>
  
   
