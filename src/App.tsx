ea5d001dfc1958c2238a32a079030fcbc1ea0edc
Kfmoss <berlin1980e@gmail.com>
2023-08-01 18:38:05 +0200

    highlight selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
     <Show above="lg">
 
-      <GridItem area={'aside'} paddingX={5}><GenreList onSelectedGenre={(genre)=> setSelectedGenre(genre)}/></GridItem>
+      <GridItem area={'aside'} paddingX={5}><GenreList selectedGenre={selectedGenre} onSelectedGenre={(genre)=> setSelectedGenre(genre)}/></GridItem>
 
     </Show>
     
